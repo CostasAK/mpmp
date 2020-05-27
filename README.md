7f40f4c2b18c3674bf658c3e2d84e65093a0f9cb
Costas <5113717-CostasAK@users.noreply.gitlab.com>
2020-05-27 21:30:46 +0200

    solved mpmp7

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ My Matt Parker's Math Puzzles solutions.
 - [The 1 Million Bank Balance Puzzle (MPMP Puzzle 6)](#the-1-million-bank-balance-puzzle-mpmp-puzzle-6)
   - [My Solution](#my-solution)
 - [Unique Distancing Puzzle (MPMP7)](#unique-distancing-puzzle-mpmp7)
+  - [My Solution](#my-solution-1)
 
 ## [The 1 Million Bank Balance Puzzle (MPMP Puzzle 6)](https://www.think-maths.co.uk/BankBalance)
 
@@ -34,3 +35,16 @@ The solution is `144` and `154`, resulting in `19` steps. Computed in approximat
 > Is it always possible to put n counters on an n x n grid, such that no two counters are the same distance apart?
 >
 > **Puzzle for Submission:** Can you place 6 counters on a 6x6 grid such that the distance between each counter is different?
+
+### My Solution
+
+Brute force by checking possible combinations with the following rules:
+
+- Use a vector of 6*6 length to place disks in.
+- Determine disk coordinates by calculating the quotient and remainder of the vector index divided by 6.
+- Calculate the Euclidean distance of all disks to each other and stop when a non-unique distance is found.
+- Break all loops once a vector of unique distances is found.
+
+The solution in given by the graphic below and it was cimputed in approximately `57` milliseconds.
+
+![My MPMP7 Solution](./mpmp7_solution.png)
--- mpmp7.js
+++ mpmp7.js
@@ -3,3 +3,69 @@
 // eslint-disable-next-line no-unused-vars
 const { PerformanceObserver, performance } = require('perf_hooks')
 var t0 = performance.now()
+
+const numberOfDisks = 6
+
+const numberOfSquares = numberOfDisks ** 2
+var coordsDisks = []
+var allUnique = true
+
+var distances = []
+var distance = []
+
+for (let disk1 = 0; disk1 < numberOfSquares - 5; disk1++) {
+  coordsDisks[0] = [Math.trunc(disk1 / numberOfDisks), disk1 % numberOfDisks]
+
+  for (let disk2 = disk1 + 1; disk2 < numberOfSquares - 4; disk2++) {
+    coordsDisks[1] = [Math.trunc(disk2 / numberOfDisks), disk2 % numberOfDisks]
+
+    for (let disk3 = disk2 + 1; disk3 < numberOfSquares - 3; disk3++) {
+      coordsDisks[2] = [Math.trunc(disk3 / numberOfDisks), disk3 % numberOfDisks]
+
+      for (let disk4 = disk3 + 1; disk4 < numberOfSquares - 2; disk4++) {
+        coordsDisks[3] = [Math.trunc(disk4 / numberOfDisks), disk4 % numberOfDisks]
+
+        for (let disk5 = disk4 + 1; disk5 < numberOfSquares - 1; disk5++) {
+          coordsDisks[4] = [Math.trunc(disk5 / numberOfDisks), disk5 % numberOfDisks]
+
+          for (let disk6 = disk5 + 1; disk6 < numberOfSquares; disk6++) {
+            coordsDisks[5] = [Math.trunc(disk6 / numberOfDisks), disk6 % numberOfDisks]
+
+            distances = []
+            allUnique = true
+
+            for (let d = 0; d < numberOfDisks - 1; d++) {
+              for (let i = d + 1; i < numberOfDisks; i++) {
+                distance = 0
+                for (let j = 0; j < coordsDisks[d].length; j++) {
+                  distance += (coordsDisks[d][j] - coordsDisks[i][j]) ** 2
+                }
+                distance = Math.sqrt(distance)
+                if (distances.includes(distance)) {
+                  allUnique = false
+                  break
+                }
+                distances.push(distance)
+              }
+              if (!allUnique) break
+            }
+          }
+          if (allUnique) break
+        }
+        if (allUnique) break
+      }
+      if (allUnique) break
+    }
+    if (allUnique) break
+  }
+  if (allUnique) break
+}
+
+var t1 = performance.now()
+
+for (let d = 0; d < numberOfDisks; d++) {
+  console.log('Disk ' + (d + 1) + ': ' + coordsDisks[d])
+}
+console.log('Distances: ' + distances)
+
+console.log('Solution computed in ' + Math.round(t1 - t0) + ' milliseconds.')
