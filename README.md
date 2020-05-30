65a238cf63d6755ff6626ef082966d790884e67d
Costas <5113717-CostasAK@users.noreply.gitlab.com>
2020-05-30 15:01:34 +0200

    generalised to NxN and finding all unique solutions

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 My Matt Parker's Math Puzzles solutions.
 
 - [The 1 Million Bank Balance Puzzle (MPMP Puzzle 6)](#the-1-million-bank-balance-puzzle-mpmp-puzzle-6)
-  - [My Solution](#my-solution)
+  - [MPMP6 Solution](#mpmp6-solution)
 - [Unique Distancing Puzzle (MPMP7)](#unique-distancing-puzzle-mpmp7)
-  - [My Solution](#my-solution-1)
+  - [MPMP7 Solution](#mpmp7-solution)
 
 ## [The 1 Million Bank Balance Puzzle (MPMP Puzzle 6)](https://www.think-maths.co.uk/BankBalance)
 
@@ -17,7 +17,7 @@ My Matt Parker's Math Puzzles solutions.
 >
 > **Puzzle for Submission:** What must your initial two deposits be to ensure that you win the million pounds?
 
-### My Solution
+### MPMP6 Solution
 
 Brute force by checking all starting deposits with the following constraints:
 
@@ -36,15 +36,18 @@ The solution is `144` and `154`, resulting in `19` steps. Computed in approximat
 >
 > **Puzzle for Submission:** Can you place 6 counters on a 6x6 grid such that the distance between each counter is different?
 
-### My Solution
+### MPMP7 Solution
 
 Brute force by checking possible combinations with the following rules:
 
 - Use a vector of 6*6 length to place disks in.
 - Determine disk coordinates by calculating the quotient and remainder of the vector index divided by 6.
 - Calculate the Euclidean distance of all disks to each other and stop when a non-unique distance is found.
-- Break all loops once a vector of unique distances is found.
+  - Start calculating distances after the 3rd disk is placed, and every following disk, so an early stop can be done when equal distances are found.
+- ~~Break all loops once a vector of unique distances is found.~~
 
-The solution in given by the graphic below and it was cimputed in approximately `57` milliseconds.
+The solution in given by the graphic below and it was computed in approximately `57` milliseconds. All solutions were found in `693` milliseconds.
 
 ![My MPMP7 Solution](./mpmp7_solution.png)
+
+2 solutions exist for the 6x6 case. 1 for the 7x7, none for 8x8 to 10x10
--- mpmp7.js
+++ mpmp7.js
@@ -1,71 +1,136 @@
 // UNIQUE DISTANCING PUZZLE - MPMP7
 
+const numberOfDisks = 6
+
 // eslint-disable-next-line no-unused-vars
 const { PerformanceObserver, performance } = require('perf_hooks')
 var t0 = performance.now()
 
-const numberOfDisks = 6
-
-const numberOfSquares = numberOfDisks ** 2
-var coordsDisks = []
-var allUnique = true
-
-var distances = []
-var distance = []
-
-for (let disk1 = 0; disk1 < numberOfSquares - 5; disk1++) {
-  coordsDisks[0] = [Math.trunc(disk1 / numberOfDisks), disk1 % numberOfDisks]
-
-  for (let disk2 = disk1 + 1; disk2 < numberOfSquares - 4; disk2++) {
-    coordsDisks[1] = [Math.trunc(disk2 / numberOfDisks), disk2 % numberOfDisks]
-
-    for (let disk3 = disk2 + 1; disk3 < numberOfSquares - 3; disk3++) {
-      coordsDisks[2] = [Math.trunc(disk3 / numberOfDisks), disk3 % numberOfDisks]
-
-      for (let disk4 = disk3 + 1; disk4 < numberOfSquares - 2; disk4++) {
-        coordsDisks[3] = [Math.trunc(disk4 / numberOfDisks), disk4 % numberOfDisks]
-
-        for (let disk5 = disk4 + 1; disk5 < numberOfSquares - 1; disk5++) {
-          coordsDisks[4] = [Math.trunc(disk5 / numberOfDisks), disk5 % numberOfDisks]
-
-          for (let disk6 = disk5 + 1; disk6 < numberOfSquares; disk6++) {
-            coordsDisks[5] = [Math.trunc(disk6 / numberOfDisks), disk6 % numberOfDisks]
-
-            distances = []
-            allUnique = true
-
-            for (let d = 0; d < numberOfDisks - 1; d++) {
-              for (let i = d + 1; i < numberOfDisks; i++) {
-                distance = 0
-                for (let j = 0; j < coordsDisks[d].length; j++) {
-                  distance += (coordsDisks[d][j] - coordsDisks[i][j]) ** 2
-                }
-                distance = Math.sqrt(distance)
-                if (distances.includes(distance)) {
-                  allUnique = false
-                  break
-                }
-                distances.push(distance)
-              }
-              if (!allUnique) break
-            }
-          }
-          if (allUnique) break
-        }
-        if (allUnique) break
-      }
-      if (allUnique) break
-    }
-    if (allUnique) break
-  }
-  if (allUnique) break
+var state = {
+  numberOfDisks: numberOfDisks,
+  numberOfSquares: numberOfDisks ** 2,
+  indicesDisks: [],
+  coordsDisks: [],
+  distances: [],
+  solutions: []
 }
+
+function getDistance (coord1, coord2) {
+  var distance = 0
+  for (let i = 0; i < coord1.length; i++) {
+    distance += (coord1[i] - coord2[i]) ** 2
+  }
+  return Math.sqrt(distance)
+}
+
+function getNewUniqueDistances (coords, distances) {
+  var newDistances = []
+  for (let i = 0; i < coords.length - 1; i++) {
+    const distance = getDistance(coords[i], coords[coords.length - 1])
+    if (distances.includes(distance) || newDistances.includes(distance)) {
+      return false
+    }
+    newDistances.push(distance)
+  }
+  return newDistances
+}
+
+function rotateSolution (coordinates, origin) {
+  var rotatedCoordinates = []
+  for (let i = 0; i < coordinates.length; i++) {
+    const coordinate = [coordinates[i][0] - origin, coordinates[i][1] - origin]
+    const rotated = [-coordinate[1], coordinate[0]]
+    rotatedCoordinates.push([rotated[0] + origin, rotated[1] + origin])
+  }
+  return rotatedCoordinates
+}
+
+function reflectSolution (coordinates, origin) {
+  var reflectedCoordinates = []
+  for (let i = 0; i < coordinates.length; i++) {
+    const coordinate = [coordinates[i][0] - origin, coordinates[i][1] - origin]
+    const reflected = [coordinate[0], -coordinate[1]]
+    reflectedCoordinates.push([reflected[0] + origin, reflected[1] + origin])
+  }
+  return reflectedCoordinates
+}
+
+function checkUniqueSolution (coordinates, state) {
+  for (let i = 0; i < state.solutions.length; i++) {
+    var countEquals = 0
+    for (let j = 0; j < coordinates.length; j++) {
+      if (state.solutions[i].coordsDisks.find(function (ele) {
+        return (JSON.stringify(ele) === JSON.stringify(coordinates[j]))
+      })) {
+        countEquals++
+      }
+      if (countEquals >= state.numberOfDisks) return false
+    }
+  }
+  return true
+}
+
+function checkNewSolution (state) {
+  var coordinates = JSON.parse(JSON.stringify(state.coordsDisks))
+  const origin = (state.numberOfDisks - 1) / 2
+
+  for (let i = 0; i < 3; i++) {
+    coordinates = rotateSolution(coordinates, origin)
+    if (!checkUniqueSolution(coordinates, state)) return false
+  }
+
+  coordinates = reflectSolution(coordinates, origin)
+  if (!checkUniqueSolution(coordinates, state)) return false
+
+  for (let i = 0; i < 3; i++) {
+    coordinates = rotateSolution(coordinates, origin)
+    if (!checkUniqueSolution(coordinates, state)) return false
+  }
+
+  return true
+}
+
+function placeDisks (state) {
+  const distancesIn = JSON.parse(JSON.stringify(state.distances))
+  const diskNumber = state.indicesDisks.length || 0
+  const maxIndexDisk = state.numberOfSquares - (state.numberOfDisks - diskNumber - 1)
+
+  state.indicesDisks.push(0)
+  state.coordsDisks.push([0, 0])
+
+  for (let disk = (state.indicesDisks[diskNumber - 1] + 1) || 0; disk < maxIndexDisk; disk++) {
+    state.indicesDisks[diskNumber] = disk
+    state.coordsDisks[diskNumber] = [Math.trunc(disk / state.numberOfDisks), disk % state.numberOfDisks]
+
+    var newDistances = getNewUniqueDistances(state.coordsDisks, state.distances)
+    if (newDistances) {
+      state.distances = state.distances.concat(newDistances)
+      if (diskNumber < state.numberOfDisks - 1) {
+        state = placeDisks(state)
+      } else if (checkNewSolution(state)) {
+        state.solutions.push({
+          coordsDisks: JSON.parse(JSON.stringify(state.coordsDisks)), distances: JSON.parse(JSON.stringify(state.distances))
+        })
+      }
+    }
+    state.distances = JSON.parse(JSON.stringify(distancesIn))
+  }
+
+  state.indicesDisks.pop()
+  state.coordsDisks.pop()
+  return state
+}
+
+state = placeDisks(state)
 
 var t1 = performance.now()
 
-for (let d = 0; d < numberOfDisks; d++) {
-  console.log('Disk ' + (d + 1) + ': ' + coordsDisks[d])
+for (let i = 0; i < state.solutions.length; i++) {
+  console.log('\n' + 'Solution ' + (i + 1))
+  for (let d = 0; d < numberOfDisks; d++) {
+    console.log('Disk ' + (d + 1) + ': ' + state.solutions[i].coordsDisks[d])
+  }
+  console.log('Distances: ' + state.solutions[i].distances)
 }
-console.log('Distances: ' + distances)
 
-console.log('Solution computed in ' + Math.round(t1 - t0) + ' milliseconds.')
+console.log('Solution(s) computed in ' + Math.round(t1 - t0) + ' milliseconds.')
