f7c83c33fd094f52c5d4a9e3bc2c5ddec3a52ed0
Costas <5113717-CostasAK@users.noreply.gitlab.com>
2020-08-10 09:22:31 +0200

    quick solution for mpmp12

---
+++ 012-marchingband/marchingband.js
@@ -0,0 +1,37 @@
+// MARCHING BAND PROBLEM - MPMP12
+
+const goalNumberOfFormations = 64
+
+// eslint-disable-next-line no-unused-vars
+const { PerformanceObserver, performance } = require('perf_hooks')
+var t0 = performance.now()
+
+var numberOfMembers = 0
+var numberOfFormations = 0
+
+// Loop until a solution is found, or a number of members is reached which results in a much larger amount of formations than the goal to prevent an endless loop.
+while(numberOfFormations != goalNumberOfFormations || numberOfFormations > 10*goalNumberOfFormations) {
+  // Reset
+  numberOfMembers = numberOfMembers + 1
+  numberOfFormations = 0
+
+  // Brute-force one half. Count non-square formations twice.
+  for (let i = 1; i <= numberOfMembers / i; i++) {
+    if (numberOfMembers / i == i) {
+      numberOfFormations = numberOfFormations + 1
+    }
+    else if (numberOfMembers % i == 0) {
+      numberOfFormations = numberOfFormations + 2
+    }
+  }
+}
+
+var t1 = performance.now()
+
+if (numberOfFormations == 64) {
+  console.log('Members: ' + numberOfMembers)
+  console.log('Solution computed in ' + Math.round(t1 - t0) + ' milliseconds.')
+}
+else {
+  console.log('No solution found. Stopped at ' + numberOfMembers + ' members after ' + Math.round(t1 - t0) + ' milliseconds.')
+}
